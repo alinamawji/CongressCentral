10fba1196dd983b6dbe24695533116511a232a8b
Trey Everson <teverson25@me.com>
2021-02-24 18:31:24 -0600

    Updating files for Flask, in process

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-# main.py
-from flask import Flask, render_template
+# application.py
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index.html')
-@app.route('/members.html/index.html/')
 def index():
     return render_template('index.html')
 
-@app.route('/members.html/', methods=['GET'])
+# MEMBERS
+@app.route('/members/', methods=['GET'])
 def members():
     return render_template('members.html')
+
+@app.route('/members/baldwin')
+def baldwin():
+    return render_template('baldwin.html')
+
+@app.route('/members/barrasso')
+def barrasso():
+    return render_template('barrasso.html')
+
+
+
+@app.route('/committees/')
+def committees():
+    return render_template('committees.html')
+
+@app.route('/legislation/')
+def legislation():
+    return render_template('legislation.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.debug = True
