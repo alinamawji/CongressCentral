d15d3c05f0b3cf18f530d7a927ace7af1196dc55
Alina Mawji <a.mawji08@gmail.com>
2021-04-20 19:29:16 -0500

    search functionality

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ ROWS_PER_PAGE = 10
 ROWS_PER_PAGE_COMMITTEE = 5
 
 # MEMBERS
-@app.route('/members/', methods=['GET'])
+@app.route('/members/', methods=['GET', 'POST'])
 def members():
     page = request.args.get('page', 1, type=int)
     member = Member.query.paginate(page, per_page=ROWS_PER_PAGE)
+    if request.method == 'POST':
+        member = Member.query.filter(Member.lname.ilike('%' + str(request.form['user_input']) + '%')).paginate(page=page, per_page=ROWS_PER_PAGE)
+    else:
+        pass
+
     return render_template('members.html', member=member)
 
 @app.route('/members/<member_id>')
@@ -31,7 +36,7 @@ def individual_member(member_id):
     return render_template('individual_member.html', member_id=member_id, member=member, committee=committee, twitter=twitter, financial=financial, industry_contributor=industry_contributor, organization_contributor=organization_contributor, sector_contributor=sector_contributor, legislation=legislation, are_part_of=are_part_of)
 
 # COMMITTEES
-@app.route('/committees/', methods=['GET'])
+@app.route('/committees/', methods=['GET','POST'])
 def committees():
     page = request.args.get('page', 1, type=int)
     committee = Committee.query.paginate(page=page, per_page=ROWS_PER_PAGE_COMMITTEE)
@@ -40,6 +45,11 @@ def committees():
     member = Member.query.all()
     subcommittees = Subcommittee.query.all()
     are_part_of = Are_Part_Of.query.all()
+    if request.method == 'POST':
+        committee = Committee.query.filter(Committee.name.ilike('%' + str(request.form['user_input']) + '%')).paginate(page=page, per_page=ROWS_PER_PAGE_COMMITTEE)
+    else:
+        pass
+
     return render_template('committees.html', committee=committee, hearing=hearing, is_pushed_through=is_pushed_through, member=member, subcommittees=subcommittees, are_part_of=are_part_of)
 
 @app.route('/committee/<committee_id>')
@@ -55,13 +65,18 @@ def individual_committee(committee_id):
     return render_template('individual_committee.html', id=committee_id, member=member, committee=committee, hearing=hearing, is_pushed_through=is_pushed_through, are_part_of=are_part_of, discuss=discuss, subcommittees=subcommittees, legislation=legislation)
 
 # LEGISLATION
-@app.route('/legislation/')
+@app.route('/legislation/', methods=['GET','POST'])
 def legislation():
     page = request.args.get('page', 1, type=int)
     legislation = Legislation.query.paginate(page=page, per_page=ROWS_PER_PAGE)
     action = Action.query.all()
     is_pushed_through = Is_Pushed_Through.query.all()
     member = Member.query.all()
+    if request.method == 'POST':
+        legislation = Legislation.query.filter(Legislation.bill_number.ilike('%' + str(request.form['user_input']) + '%')).paginate(page=page, per_page=ROWS_PER_PAGE)
+    else:
+        pass
+
     return render_template('legislation.html', legislation=legislation, member=member, action=action, is_pushed_through=is_pushed_through)
 
 @app.route('/legislation/<bill_id>')
